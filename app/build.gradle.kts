740f2985f1ac872968c3b090170f38981cd6b6f1
rj <rj@gigioberto.com>
2024-10-11 12:28:01 -0300

    configs ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,19 @@
+import java.util.Properties
+val properties: Properties = Properties()
+
+properties.load(
+    project.rootProject
+        .file("local.properties")
+        .inputStream()
+)
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    //   added
+    alias(libs.plugins.android.dagger.hilt)
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -8,21 +21,31 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "cl.rodrigo_javier_garrido_dagle.mobileapplydigitaltest"
+        applicationId = "cl.rodrigo_javier_garrido_dagle.mobiledevtest"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
         vectorDrawables {
             useSupportLibrary = true
         }
+
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+            isMinifyEnabled = false
+        }
+
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -38,6 +61,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -45,7 +69,56 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/licenses/**"
+            excludes += "META-INF/AL2.0"
+            excludes += "META-INF/LGPL2.1"
         }
+    }
+
+    flavorDimensions += "version"
+//    productFlavors {
+//
+//        create("Test") {
+//            applicationIdSuffix = ".Test"
+//            versionNameSuffix = "1_Test"
+//
+//            resValue(
+//                "string",
+//                "app_name",
+//                "Rodrigo Javier test"
+//            )
+//
+//            buildConfigField(
+//                "String",
+//                "URL_BASE",
+//                properties.getProperty("URL_BASE")
+//            )
+//
+//            buildConfigField(
+//                "String",
+//                "URL_ENDPOINT",
+//                properties.getProperty("URL_ENDPOINT")
+//            )
+//
+//        }
+//
+//    }
+
+    sourceSets {
+//        this.getByName("androidTest") {
+//            resources.srcDirs("src/androidTest/resources", "src/sharedTest/resources")
+//        }
+//        this.getByName("test") {
+//            resources.srcDirs("src/test/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest")
+//        }
+    }
+//    testOptions {
+//        unitTests.isReturnDefaultValues = true
+//    }
+// app/src/test/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/domain/usecases/HitUsecasesTest.kt
+// app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobiledevtest/domain/usecases/HitUsecases.kt
+    hilt {
+        enableAggregatingTask = true
     }
 }
 
@@ -59,11 +132,74 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+// tests
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+//   added tests MOKK y mockito
+    testImplementation(libs.mokk)
+    testImplementation(libs.mockitocore)
+    testImplementation(libs.mockitoinline)
+
+// TEST
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+//
+
+
+//    testImplementation("junit:junit:4.12")
+//    testImplementation("org.mockito:mockito-core:2.24.5")
+
+
+//   picasso
+    implementation(libs.androidx.picasso)
+//    retrofit
+    implementation(libs.androidx.retrofit)
+    implementation(libs.androidx.retrofitgson)
+// okhttp
+    implementation(libs.okhttp)
+    implementation(libs.googlegson)
+    implementation(libs.okhttp.interceptor)
+//    coroutines
+    implementation(libs.androidx.coroutines)
+    implementation(libs.androidx.coroutinesandroid)
+//   coroutine lifecycle scopes
+    implementation(libs.androidx.activityktx)
+    implementation(libs.androidx.lifecyclesextensions)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+// KTX - Viewmodel Y Livedata NO ACTUALIZAR: 2.5.1
+    implementation(libs.androidx.lifecycle.livedata)
+//    data store
+    implementation(libs.androidx.datastore)
+//Room
+    implementation(libs.room)
+    kapt(libs.roomkaptcompiler)
+    implementation(libs.androidx.room.runtime)
+//Hilt
+    implementation(libs.dagger.google)
+    implementation(libs.androidx.hilt.navigation.compose.v120)
+    annotationProcessor(libs.androidx.google.compiler)
+    kapt(libs.androidx.google.dagger.kapt.android)
+    kapt(libs.androidx.google.compiler)
+    kapt(libs.androidx.hilt.compiler.v120)
+    //
+//    testImplementation("android.arch.core:core-testing:1.1.1")
+
 }
+
+// avoid the issues related NonExistentClass
+kapt { correctErrorTypes = true }
