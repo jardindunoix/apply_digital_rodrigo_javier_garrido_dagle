c4342e17de311f7a34286368baba27150c4f39b5
rj <rj@gigioberto.com>
2024-10-11 13:50:29 -0300

    mappers ready

--- app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobileapplydigitaltest/domain/mappers/NetworkMapper.kt
+++ app/src/main/java/cl/rodrigo_javier_garrido_dagle/mobileapplydigitaltest/domain/mappers/NetworkMapper.kt
@@ -1,7 +1,8 @@
-package cl.rodrigo_javier_garrido_dagle.mobiledevtest.domain.mappers
+package cl.rodrigo_javier_garrido_dagle.mobileapplydigitaltest.domain.mappers
 
-import cl.rodrigo_javier_garrido_dagle.mobiledevtest.data.database.entities.HitEntity
-import cl.rodrigo_javier_garrido_dagle.mobiledevtest.data.network.dto.HitDto
+import cl.rodrigo_javier_garrido_dagle.mobileapplydigitaltest.data.database.entities.HitEntity
+import cl.rodrigo_javier_garrido_dagle.mobileapplydigitaltest.data.network.dto.HitDto
+import cl.rodrigo_javier_garrido_dagle.mobileapplydigitaltest.domain.model.Hit
 
 fun HitDto.networkToDomain() = Hit(
     tags = tags,
